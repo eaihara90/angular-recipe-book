4c38e1f0ba814d3ed0be015ef7ee840b2b262da3
Eduardo Aihara <eaihara90@gmail.com>
2020-10-19 21:31:21 -0300

    hidden api files

--- src/environments/api.env.ts
+++
@@ -1,5 +0,0 @@
-export const apiKeys = {
-    signupUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
-    loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
-    webApiKey: 'AIzaSyDC0HQJthz57f26nXlHBRugWuzpGkIZL7Q',
-};
